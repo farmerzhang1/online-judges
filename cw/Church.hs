8e0d87dc91461da19dd38c379aa564a20fc2ff40
farmerzhang1 <farmerzhang1@mail.ustc.edu.cn>
2021-10-04 11:47:18 +0800

    fix type errors

--- cw/Church.hs
+++ cw/Church.hs
@@ -2,14 +2,22 @@
 module Church where
 
 newtype Church = Church { runChurch :: forall a. (a -> a) -> a -> a }
+-- please see the question I posed in
+-- https://stackoverflow.com/questions/69426296/church-numerals-rigid-type-and-infinite-type
 
 pred1 :: Church -> Church
 pred1 (Church n) = Church (\f a -> extract (n (\g h -> h (g f)) (const a))) where
   extract k = k id
 
-pred2 :: forall a. ((a -> a) -> a -> a) -> (a -> a) -> a -> a
+pred2 :: (forall a. (a -> a) -> a -> a) -> (b -> b) -> b -> b
 pred2 n = runChurch $ pred1 (Church n)
 
-pred3 :: forall a. ((a -> a) -> a -> a) -> (a -> a) -> a -> a
-pred3 n f a1 = extract (n (\g h -> h (g f)) (const a1)) where
+pred3 :: (forall a. (a -> a) -> a -> a) -> (b -> b) -> b -> b
+pred3 n f b = extract (n (\g h -> h (g f)) (const b)) where
   extract k = k id
+
+-- test1 :: Show s => s
+-- test1 = "asdasd"
+
+test :: Num a => a
+test = 42 -- 42 can be any instances of Num
